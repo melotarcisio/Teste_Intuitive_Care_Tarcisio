8f073161129ce96a3c191f2bef69fb3a656d7cc9
melotarcisio <melotarcisio@hotmail.com>
2021-06-07 19:31:18 +0000

    sa

--- Tarefa_2.py
+++ Tarefa_2.py
@@ -9,9 +9,9 @@ import pandas
 
 df = read_pdf('Componente_Organizacional.pdf',pages=79)
 df = pandas.DataFrame(df[0])
-table1 = df.to_csv(encoding="utf-8",)
+table1 = df.to_csv()
 #new_file('Tabela1.csv',table1)
 #print(table1.content)
 
 compression_opts = dict(method='zip',archive_name='out.csv')  
-df.to_csv('out.zip', index=False,compression=compression_opts)  
+df.to_csv('out.zip',encoding="utf-8",compression=compression_opts)  
