9322e218d9db4defe097be704a9367fe718b909b
melotarcisio <melotarcisio@hotmail.com>
2021-06-07 23:08:01 +0000

    Tabela 2 funcionando

--- Tarefa_2.py
+++ Tarefa_2.py
@@ -2,25 +2,59 @@ from tabula import read_pdf,convert_into
 import pandas
 import csv
 
+#Salvando as tabelas
+D_F = read_pdf('Componente_Organizacional.pdf',pages=79)
+D_F2 = read_pdf('Componente_Organizacional.pdf',pages=[79,80,81,82,83,84])
+D_F3 = read_pdf('Componente_Organizacional.pdf',pages=85)
+
 def create_csv(file_content,file_name):
     new_file = open(file_name,'w')
-    #file_content = csv.writer(file_contet)
     new_file.write(file_content)
     print('Arquivo criado com sucesso')
 
-#Salvando as tabelas
-df = read_pdf('Componente_Organizacional.pdf',pages=[79,80,81,82,83,84,85])
-#Separando a primeira tabela
-df1 = pandas.DataFrame(df[0])
-df1 = df1.to_csv(sep=';',encoding='utf-8')
-#Separando a segunda tabela
-df2 = pandas.DataFrame(df[1])
+def repair_name(tbl):
+    tbl = tbl.drop(0)
+    tbl.columns = ['Código','Descrição da categoria']
+    return tbl
 
-df3 = pandas.DataFrame(df[2])
-#table1=df.to_csv(index=False,encoding="utf-8")  
-#print(df2)
-create_csv(df1,'table1.csv')
+def main():
 
-compression_opts = dict(method='zip',archive_name='out.csv')
-#df1.to_csv('out.zip',sep=';',compression=compression_opts)
-print(df1)
+
+    #Separando a primeira tabela
+    df1 = pandas.DataFrame(D_F[0])
+    #df1 = repair_name(df1)
+    #df1 = df1.to_csv(sep=';',encoding='utf-8')
+
+    #Separando a segunda tabela
+    i = [None,None,None,None,None,None]
+    i[0] = pandas.DataFrame(D_F2[1])
+    i[0].columns = ['Código','Descrição da categoria']
+    i[0] = i[0].drop(0)
+    i[0].index=i[0].index-1
+    j = 1
+    df2 = i[0]
+    while j<6:
+        i[j] = pandas.DataFrame(D_F2[j+1])
+        i[j].loc[-1] = i[j].columns
+        i[j].columns = ['Código','Descrição da categoria']
+        i[j].index = i[j].index + df2.shape[0] +1
+        i[j] = i[j].sort_index()
+        df2 = pandas.concat([df2,i[j]],axis=0)
+        j = j+1
+    df2 = df2.to_csv(sep=';',encoding='utf-8')  
+    #print(j)
+
+  
+ 
+    create_csv(df2,'table2.csv')
+
+    
+    
+    
+    
+    
+    #compression_opts = dict(method='zip',archive_name='out.csv')
+    #df1.to_csv('out.zip',sep=';',compression=compression_opts)
+
+if __name__=='__main__':    
+    main()
